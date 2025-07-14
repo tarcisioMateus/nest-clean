51412d3bb19f3d3c5693c046274e443edefc951c
Tarcísio Mateus <tarcisiomateus98@gmail.com>
2025-07-14 16:07:49 -0300

    ./test/setup-e2e

---
+++ test/setup-e2e.ts
@@ -0,0 +1,32 @@
+import { PrismaClient } from '@prisma/client'
+import 'dotenv/config'
+import { execSync } from 'node:child_process'
+import { randomUUID } from 'node:crypto'
+
+const prisma = new PrismaClient()
+
+function generateDatabaseUrl(schema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('Please provide a DATABASE_URL env variable')
+  }
+
+  const url = new URL(process.env.DATABASE_URL)
+
+  url.searchParams.set('schema', schema)
+
+  return url.toString()
+}
+
+const schema = randomUUID()
+
+beforeAll(async() => {
+  process.env.DATABASE_URL = generateDatabaseUrl(schema)
+
+  execSync('npx prisma migrate deploy')
+})
+
+afterAll(async() => {
+  await prisma.$executeRawUnsafe(`DROP SCHEMA IF EXISTS "${schema}" CASCADE`,)
+
+  await prisma.$disconnect()
+})
